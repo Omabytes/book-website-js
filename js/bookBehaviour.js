fba51a0394d59f5fb21b0383cb23d9a0722f0701
omabytes <321.trebor@gmail.com>
2018-10-18 14:14:35 +0100

    js code format tidy up

--- js/bookBehaviour.js
+++ js/bookBehaviour.js
@@ -1,48 +1,52 @@
 function handleBookHover() {
-    if(!this.classList.contains("viewed")) this.classList.add("hovered")
-  }
-  
-  function handleBookUnhover() {
+    if (!this.classList.contains("viewed")) this.classList.add("hovered")
+}
+    
+function handleBookUnhover() {
     this.classList.remove("hovered")
-  }
-  
-  function handleBookClick(e) {
-    if(e.which !== 1) return
-    let moving = false
-    this.addEventListener("mousemove", () => {
-      moving = true
-    })
-    let mouseUpEventRemove
+}
+    
+function handleBookClick(e) {
+    if (e.which !== 1) return  // if keypress is NOT mouse1
+
+    let mouseUpEventRemove 
     let scrollToBookRemove
+    let moving = false
+    
+    this.addEventListener("mousemove", () => {
+        moving = true
+    })
+
     const mouseUpEvent = () => {
-      if(!this.classList.contains("viewed")) {
-        this.classList.add("viewed")
-        this.addEventListener("transitionend", scrollToBook)
-      } else if(!moving) {
-        alert("link to book page")
-      }
-      mouseUpEventRemove()
+        if (!this.classList.contains("viewed")) {
+            this.classList.add("viewed")
+            this.addEventListener("transitionend", scrollToBook)
+        } else if (!moving) {
+            alert("link to book page")
+        }
+        mouseUpEventRemove()
     }
     const scrollToBook = () => {
-      this.scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
-      scrollToBookRemove()
+        this.scrollIntoView({behavior: "smooth", block: "center", inline: "center"})
+        scrollToBookRemove()
     }
     mouseUpEventRemove = () => this.removeEventListener("mouseup", mouseUpEvent)
     scrollToBookRemove = () => this.removeEventListener("transitionend", scrollToBook)
     this.addEventListener("mouseup", mouseUpEvent)
-  }
-  
-  document.addEventListener("mousedown", function(e) {
-    if(e.which !== 1) return
+}
+
+document.addEventListener("mousedown", function(e) {
+    if (e.which !== 1) return  // if keypress is NOT mouse1
+
     let element = document.querySelector(".item.viewed")
-    if(element !== null) {
-      let isClickInside = element.contains(e.target)
-      if(!isClickInside) {
-        element.classList.remove("viewed")
-      }
+    if (element !== null) {
+        let isClickInside = element.contains(e.target)
+        if (!isClickInside) {
+            element.classList.remove("viewed")
+        }
     }
-  })
-  books.forEach(book => book.addEventListener("mouseover", handleBookHover))
-  books.forEach(book => book.addEventListener("mouseleave", handleBookUnhover))
-  books.forEach(book => book.addEventListener("mousedown", handleBookClick))
-  
+})
+
+books.forEach(book => book.addEventListener("mouseover", handleBookHover))
+books.forEach(book => book.addEventListener("mouseleave", handleBookUnhover))
+books.forEach(book => book.addEventListener("mousedown", handleBookClick))
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,12 +1,14 @@
-function smoothScroll(offsetY,time) {
-    if( !offsetY) return
-    var to = offsetY
-    var from = window.scrollY
-    var start = new Date().getTime(),
+function smoothScroll(offsetY, time) {
+    if (!offsetY) return
+    let to = offsetY
+    let from = window.scrollY
+    let start = new Date().getTime(),
         timer = setInterval(function() {
-            var step = Math.min(1,(new Date().getTime()-start)/time)
-            window.scrollTo(0,(from+step*(to-from))+1)
-            if( step == 1){ clearInterval(timer);}
-        } ,1)
-    window.scrollTo(0,(from+1))
+            let step = Math.min(1, (new Date().getTime() - start) / time)
+            window.scrollTo(0, (from + step * (to - from)) + 1)
+            if (step === 1) {
+                clearInterval(timer)
+            }
+        }, 1)
+    window.scrollTo(0, (from + 1))
 }
